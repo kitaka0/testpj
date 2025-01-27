0b717a0d9249961f84f98efe5fcb8f69303393a3
ofumin <ofmi16@gmail.com>
2025-01-27 21:50:58 +0900

    ホーム画面の改善
    - 背景画像の設定
    - 右上にボタン2個設置（内容はてきとー）

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,27 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -38,15 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -59,11 +35,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -74,9 +45,39 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: 'Settings',
+            onPressed: () {
+              // 設定ボタンを押したときの処理
+              if (kDebugMode) {
+                print('Settings button pressed');
+              }
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.logout),
+            tooltip: 'Logout',
+            onPressed: () {
+              // ログアウトボタンを押したときの処理
+              if (kDebugMode) {
+                print('Logout button pressed');
+              }
+            },
+          ),
+        ],
       ),
       body: Stack(
         children: [
+          // 背景画像
+          SizedBox.expand(
+            child: Image.asset(
+              'assets/background.jpg', // 画像ファイルのパス
+              fit: BoxFit.cover, // 画面全体を覆う
+            ),
+          ),
+          // メインコンテンツ
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -89,14 +90,14 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
           ),
+          // 左下のボタン
           Positioned(
-            bottom: 16, // 下端からの位置
-            left: 16, // 左端からの位置
+            bottom: 16,
+            left: 16,
             child: FloatingActionButton(
               onPressed: () {
-                // 左下のボタンを押したときの処理
                 setState(() {
-                  _counter += 2; // 例としてカウンターを2増やす処理
+                  _counter += 2; // カウンターを2増やす
                 });
               },
               tooltip: 'Left Button',
@@ -109,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // 右下のボタン
+      ),
     );
   }
 }
